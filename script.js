5f3da46fa386ecc302b0c0a926e89cf1567c81d4
megacrow70 <saiannacone14@gmail.com>
2026-05-03 13:21:10 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function updatePage() {
+function userInput() {
+    //Get user input values
+    let fnameInput = document.getElementById("firstName").value;
+    let lnameInput = document.getElementById("lastName").value;
+    let emailInput = document.getElementById("email").value;
+    let addressInput = document.getElementById("contact-info").value;
 
-// Get user input values
-let nameInput = document.getElementById("userName").value;
-let messageInput = document.getElementById("messText").value;
-let selectedColor = document.getElementById("backgroundColor").value;
+    //Return response upon completion of form
 
-// Update greeting if name is entered
-if (nameInput !== "") {
-document.getElementById("userGreeting").innerHTML = "Hello " + nameInput + "!";
-}
+    function showAlert(alertId) {
+        const bootstrapAlert = document.querySelector("#" + alertId)
+        const collapse = new bootstrap.Collapse(bootstrapAlert);
+        collapse.show();
+    }
 
-// Update message if message is entered
-if (messageInput !== "") {
-document.getElementById("userMessage").innerHTML = messageInput;
-}
-
-// Update background color if selected
-if (selectedColor !== "") {
-document.body.style.backgroundColor = selectedColor;
-}
-
+    const modalYesBtn = document.querySelector("#contact-modal .btn-primary");
+    modalYesBtn.addEventListener("click", function (){
+        const sendBtn = document.querySelector("send-form-btn")
+        sendBtn.computedStyleMap.display = "none";
+        showAlert("conf-alert");
+    });
 }
